859381a8ddc1843af3bd1ce06567cfef4b6f6fd2
Onpriyapangpond <s6304062620142@email.kmutnb.ac.th>
2024-05-30 14:54:44 +0700

    Test

--- ข้อ1/1.py
+++ ข้อ1/1.py
@@ -1,1 +1,12 @@
-print('fff')
+def factorial(n):
+    if n == 0 or n == 1:
+        return 1
+    else:
+        return n * factorial(n-1)
+
+
+number = int(input("กรุณาใส่ตัวเลขเพื่อหาค่า factorial: "))
+
+
+result = factorial(number)
+print(f"{number}! = {result}")
